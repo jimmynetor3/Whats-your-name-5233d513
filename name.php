d7786ca7f1775b8a43fbb72538421782807f52a1
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2019-10-30 09:50:25 +0100

    uitwerking van opdracht

--- name.php
+++ name.php
@@ -1,12 +1,12 @@
 <?php 
-$antwoord1 = readline("wat is je voornaam" . PHP_EOL); 
+$antwoord1 = readline("wat is je voornaam" . PHP_EOL);
 
 echo $antwoord1;
 
-$antwoord2 = readline("wat is je achternaam" . PHP_EOL);  
+$antwoord2  = readline("wat is je achternaam" . PHP_EOL);  
 
-echo $antwoord2;
+echo $antwoord2 . PHP_EOL;
 
-print ("jouw naam is" . $antwoord1 . $antwoord2 . PHP_EOL);
+print ("jouw naam is" . PHP_EOL . $antwoord1 ." ". $antwoord2 . PHP_EOL);
 
  ?>
